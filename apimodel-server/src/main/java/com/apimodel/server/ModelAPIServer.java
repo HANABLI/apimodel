31abf51344bde9beec233a38c105fe29b05de5b4
Hatem Nabli <hatem.nabli@actemium.com>
2024-02-08 23:14:11 +0100

    Embedding Jetty as a Servlet Container

--- apimodel-server/src/main/java/com/apimodel/server/ModelAPIServer.java
+++ apimodel-server/src/main/java/com/apimodel/server/ModelAPIServer.java
@@ -1,20 +1,26 @@
 package com.apimodel.server;
 
 
+import jakarta.servlet.ServletContext;
 import org.eclipse.jetty.server.*;
+import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.resource.Resource;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
+import org.glassfish.jersey.servlet.ServletContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static org.eclipse.jetty.http.HttpScheme.HTTPS;
 import static org.eclipse.jetty.http.HttpVersion.HTTP_1_1;
+import static org.eclipse.jetty.servlet.ServletContextHandler.NO_SESSIONS;
 
 public class ModelAPIServer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ModelAPIServer.class);
 
     public static void main(String...  args) throws Exception {
-        LOGGER.info("Hello World");
 
         HttpConfiguration httpConfiguration = new HttpConfiguration();
         httpConfiguration.setSecureScheme(HTTPS.asString());
@@ -39,6 +45,19 @@ public class ModelAPIServer {
 
         server.addConnector(httpsConnector);
 
+        ServletContextHandler servletContextHandler = new ServletContextHandler(NO_SESSIONS);
+        servletContextHandler.setContextPath("/");
+        servletContextHandler.setBaseResource(Resource.newResource("apimodel-server/src/main/resources/www"));
+        servletContextHandler.addServlet(DefaultServlet.class, "/");
+
+        server.setHandler(servletContextHandler);
+
+        ServletHolder apiServletHolder = servletContextHandler.addServlet(ServletContainer.class, "/api/*");
+
+
+        apiServletHolder.setInitParameter("jakarta.ws.rs.Application", "TODO");
+
+        LOGGER.info("Server starting");
         server.start();
         server.join();
     }
