5995f5fb63ed6997034e3ef9c3662648904e320f
Hatem Nabli <hatem.nabli@actemium.com>
2024-02-12 17:10:44 +0100

    CorsFilter Unit Test

--- apimodel-rest/src/test/java/com/apimodel/rest/security/CorsFilterTest.java
+++ apimodel-rest/src/test/java/com/apimodel/rest/security/CorsFilterTest.java
@@ -1,4 +1,26 @@
 package com.apimodel.rest.security;
 
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.core.MultivaluedHashMap;
+import jakarta.ws.rs.core.MultivaluedMap;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.mockito.Mockito;
+
 public class CorsFilterTest {
+    @Test
+    public void testFilter() {
+        MultivaluedMap<String, Object> headerMap = new MultivaluedHashMap<>();
+        ContainerResponseContext containerRequestContext = Mockito.mock(ContainerResponseContext.class);
+        Mockito.when(containerRequestContext.getHeaders()).thenReturn(headerMap);
+
+        new CorsFilter().filter(null, containerRequestContext);
+
+        Assertions.assertEquals(3, headerMap.size());
+        Assertions.assertTrue(headerMap.containsKey("Access-Control-Allow-Origin"));
+        Assertions.assertTrue(headerMap.containsKey("Access-Control-Allow-Methods"));
+        Assertions.assertEquals("[*]", headerMap.get("Access-Control-Allow-Origin").toString());
+        Assertions.assertEquals("[GET, PUT, POST, DELETE, HEAD, OPTIONS, PATCH]",
+                headerMap.get("Access-Control-Allow-Methods").toString());
+    }
 }
